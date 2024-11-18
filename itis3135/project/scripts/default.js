15784b9661053a2bd06bdfbb57c8cc365afa7e49
Quinn Migliaccio <qmigliac@uncc.edu>
2024-11-18 18:06:49 -0500

    lot of stuff

---
+++ itis3135/project/scripts/default.js
@@ -0,0 +1,9 @@
+document.getElementById("nav-link").addEventListener("click", function(event) { //for hq page
+    // Show a confirmation alert
+    let confirmLeave = confirm("Are you sure you want to leave this page? You will be directed the the offical GWC website.");
+    if (!confirmLeave) {
+        // Prevent navigation if the user cancels
+        event.preventDefault();
+    }
+});
+
